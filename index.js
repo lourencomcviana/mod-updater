6ef8ca38880614ee55547a62183e5876a73ec3d0
lourencomcviana <lourenco.m.c.viana@gmail.com>
2023-11-20 22:38:27 -0300

    added colors

--- index.js
+++ index.js
@@ -33,19 +33,37 @@ if (process.argv[2]) {
 else {
     console.log('no base path passed, default is execution path');
 }
+// https://stackoverflow.com/questions/9781218/how-to-change-node-jss-console-font-color
+const Reset = "\x1b[0m";
+const FgMagenta = "\x1b[35m";
+const FgBlack = "\x1b[30m";
+const BgGreen = "\x1b[42m";
+const BgYellow = "\x1b[43m";
 const jointPath = path.posix.join(basePath, '**/.git');
-console.log('search path: ' + jointPath);
-(0, glob_1.glob)(jointPath, { ignore: ['node_modules/**', 'updater/.git'] })
+console.log(FgMagenta + 'Updating files on path: ' + Reset + jointPath);
+(0, glob_1.glob)(jointPath, { ignore: ['node_modules/**', 'updater/.git', '.git'] })
     .then((res) => {
+    console.log(res);
     res.forEach(item => {
         const dir = path.parse(item).dir;
         const git = (0, simple_git_1.simpleGit)(dir, { binary: 'git' });
         git.status((err, res) => {
-            if (!err && res.isClean()) {
+            if (err) {
+                console.error(err);
+            }
+            else if (res.isClean()) {
                 git.pull((pullErr, callStatus) => {
-                    console.log('updated ' + dir);
-                    console.log(callStatus.summary);
+                    if (pullErr) {
+                        console.error(pullErr);
+                    }
+                    else {
+                        console.log(BgGreen + 'updated ' + dir + Reset);
+                        console.log(callStatus.summary);
+                    }
                 });
+            }
+            else {
+                console.log(BgYellow + FgBlack + 'is not clean ' + dir + Reset);
             }
         });
     });
--- index.ts
+++ index.ts
@@ -11,23 +11,39 @@ if(process.argv[2]){
     console.log('no base path passed, default is execution path')
 }
 
+// https://stackoverflow.com/questions/9781218/how-to-change-node-jss-console-font-color
+const Reset = "\x1b[0m"
+const FgMagenta = "\x1b[35m"
+const FgBlack = "\x1b[30m"
+const BgGreen = "\x1b[42m"
+const BgYellow = "\x1b[43m"
+
+
 
 const jointPath = path.posix.join(basePath,'**/.git');
-console.log('search path: '+jointPath)
-glob(jointPath, { ignore: ['node_modules/**','updater/.git'] })
+console.log(FgMagenta+'Updating files on path: '+Reset+jointPath)
+glob(jointPath, { ignore: ['node_modules/**','updater/.git', '.git'] })
     .then((res) =>{
-       
         res.forEach(item =>{
             const dir = path.parse(item).dir
             const git: SimpleGit = simpleGit(dir, { binary: 'git' });
             
             git.status((err,res) =>{
                 
-                if(!err && res.isClean()) {
+                if(err) {
+                    console.error(err)
+                }
+                else if(res.isClean()) {
                     git.pull((pullErr, callStatus) =>{
-                        console.log('updated '+ dir);
-                        console.log(callStatus.summary)
+                        if(pullErr){
+                            console.error(pullErr)
+                        } else {
+                            console.log(BgGreen+'updated '+ dir+Reset);
+                            console.log(callStatus.summary)
+                        }
                     })
+                } else {
+                    console.log(BgYellow+FgBlack+'is not clean '+ dir+Reset);
                 }
             })
         })
